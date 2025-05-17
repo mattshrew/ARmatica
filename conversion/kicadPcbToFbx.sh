da7b3d6bfc29650a92a0f370eed493121c5bc186
Daniel Zhang <d.zhang200788@gmail.com>
2025-05-17 19:24:53 -0400

    server :fish::fish::fish::fish::fish::fish::fish::fish::fish::fish::fish::fish:

--- conversion/kicadPcbToFbx.sh
+++ conversion/kicadPcbToFbx.sh
@@ -2,8 +2,22 @@
 if [ -z "$1" ]; then
     echo "put a file in dummy"
 else
-    # Replace these with the paths to the binaries
-    /Applications/KiCad/KiCad.app/Contents/MacOS/kicad-cli pcb export step $1
-    /Applications/FreeCAD.app/Contents/Resources/bin/freecad ./stepToGltfFreeCAD.py
-    /Applications/Blender.app/Contents/MacOS/Blender --background --python ./gltfToFbxBlender.py
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        prev=$PWD
+        cd "${0%/*}"
+        # Replace these with the paths to the binaries
+        /Applications/KiCad/KiCad.app/Contents/MacOS/kicad-cli pcb export step "$1"
+        /Applications/FreeCAD.app/Contents/Resources/bin/freecad ./stepToGltfFreeCAD.py
+        /Applications/Blender.app/Contents/MacOS/Blender --background --python ./gltfToFbxBlender.py
+        if [ -n "$2" ]; then
+            timestamp=$(date +"%Y%m%d%H%M")
+            mv model.fbx "$prev/$2/model_$timestamp.fbx"
+        fi
+        # elif [[ "$OSTYPE" == "msys" ]]; then
+        # TODO later
+        # /Applications/KiCad/KiCad.app/Contents/MacOS/kicad-cli pcb export step $1
+        # /Applications/FreeCAD.app/Contents/Resources/bin/freecad ./stepToGltfFreeCAD.py
+        # /Applications/Blender.app/Contents/MacOS/Blender --background --python ./gltfToFbxBlender.py
+    fi
 fi
