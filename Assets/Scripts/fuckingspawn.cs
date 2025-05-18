f00ca0bcd373a3b6469d825397febc41c51412d2
Amit-Weis <amitweisgor@gmail.com>
2025-05-17 22:01:10 -0400

    fuck :fish: :fish: :fish: :fish: :fish: :fish: :fish: :fish: :fish: :fish: :fish: :fish: :fish: :fish: :fish: :fish:

---
+++ Assets/Scripts/fuckingspawn.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using TriLibCore;
+using TriLibCore.General;
+using UnityEngine;
+
+
+public class fuckingspawn : MonoBehaviour
+{ // Example path on Android
+    public TrackedImageSchematicLoader schematicLoader;
+    // private string modelFolderPath = $"/storage/emulated/0/Download/ARmatica/{schematicLoader.selectedSchematicName}";
+    private string modelFilePath = "/storage/emulated/0/Download/ARmatica/model_202505172053.fbx";
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UnityEngine.Debug.Log($"Attempting to load model from path: {modelFilePath}");
+
+        var assetLoaderOptions = AssetLoader.CreateDefaultLoaderOptions();
+
+        AssetLoader.LoadModelFromFile(modelFilePath, OnLoad, OnMaterialsLoad, OnProgress, OnError, null, assetLoaderOptions);
+    }
+
+
+    private void OnProgress(AssetLoaderContext assetLoaderContext, float progress)
+    {
+        UnityEngine.Debug.Log($"Loading progress: {progress * 100f}%");
+    }
+
+    private void OnError(IContextualizedError contextualizedError)
+    {
+        UnityEngine.Debug.LogError($"Error loading model: {contextualizedError.GetInnerException()}");
+    }
+
+    private void OnLoad(AssetLoaderContext assetLoaderContext)
+    {
+        var loadedGameObject = assetLoaderContext.RootGameObject;
+        loadedGameObject.SetActive(false);
+    }
+
+    private void OnMaterialsLoad(AssetLoaderContext assetLoaderContext)
+    {
+        var loadedGameObject = assetLoaderContext.RootGameObject;
+        CleanUpImportedModel(loadedGameObject);
+        ConvertMaterialsToURPLit(loadedGameObject);
+        loadedGameObject.SetActive(true);
+        loadedGameObject.transform.position = Vector3.zero;
+    }
+
+    private void CleanUpImportedModel(GameObject root)
+    {
+        string[] namesToDelete = { "cube", "camera", "light" };
+
+        for (int i = root.transform.childCount - 1; i >= 0; i--)
+        {
+            Transform child = root.transform.GetChild(i);
+            foreach (string name in namesToDelete)
+            {
+                if (child.name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                {
+                    DestroyImmediate(child.gameObject);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void ConvertMaterialsToURPLit(GameObject root)
+    {
+        Shader urpLitShader = Shader.Find("Universal Render Pipeline/Lit");
+        if (urpLitShader == null)
+        {
+            UnityEngine.Debug.LogError("URP Lit Shader not found. Ensure URP is installed and set up correctly.");
+            return;
+        }
+
+        foreach (Renderer renderer in root.GetComponentsInChildren<Renderer>(true))
+        {
+            Material[] newMaterials = new Material[renderer.sharedMaterials.Length];
+
+            for (int i = 0; i < renderer.sharedMaterials.Length; i++)
+            {
+                Material originalMat = renderer.sharedMaterials[i];
+                if (originalMat == null) continue;
+
+                Material newMat = new Material(urpLitShader);
+
+                if (originalMat.HasProperty("_MainTex"))
+                    newMat.SetTexture("_BaseMap", originalMat.GetTexture("_MainTex"));
+
+                if (originalMat.HasProperty("_Color"))
+                    newMat.SetColor("_BaseColor", originalMat.GetColor("_Color"));
+
+                newMaterials[i] = newMat;
+            }
+
+            renderer.sharedMaterials = newMaterials;
+        }
+    }
+}
